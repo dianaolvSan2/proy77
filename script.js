2b041ec0d14856d857f17b29c6adefde41dd7837
dianaolvSan2 <59063498+dianaolvSan2@users.noreply.github.com>
2022-07-22 18:42:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Mi primer proyecto en la consola");
+
+console.log("¿QUE ES VISUAL STUDIO CODE Y PARA QUE SIVE?  Visual Studio Code es un editor de código fuente ligero pero potente que se ejecuta en su escritorio y está disponible para Windows, macOS y Linux. Viene con soporte integrado para JavaScript, TypeScript y Node.js y tiene un rico ecosistema de extensiones para otros lenguajes y tiempos de ejecución (como C++, C#, Java, Python, PHP, Go, .NET). ")
+
+            var primer_nombre = "Diana";
+            var apellido = "Olvera";
+
+            var nombre_completo = primer_nombre.concat(apellido);
+            console.log(nombre_completo);
+
+            var a = 10;
+            var b = 5;
+
+            var sumar = a + b ;
+            console.log(sumar);
+
+            var restar = a - b ;
+            console.log(restar);
+
+            var multiplicar = a * b ;
+            console.log(multiplicar);
+
+            var dividir = a / b ;
+            console.log(dividir);
